bb78bbd35dd6ebd9fa474cd59e96722d9dbcfb53
Dominik Skowron <56132989+donnik007@users.noreply.github.com>
2025-07-10 09:08:33 +0000

    xd

--- tests/test_app.js
+++ tests/test_app.js
@@ -1,8 +1,9 @@
 const {Builder, By, until} = require('selenium-webdriver');
 (async function example() {
-  let driver = await new Builder().forBrowser('chrome').usingServer('http://selenium:4444/wd/hub').build();
+  // Use localhost since PHP server runs on 127.0.0.1:8000 in CI
+  let driver = await new Builder().forBrowser('chrome').usingServer('http://localhost:4444/wd/hub').build();
   try {
-    await driver.get('http://web:80');
+    await driver.get('http://127.0.0.1:8000');
     let heading = await driver.findElement(By.tagName('h1')).getText();
     console.log('Heading:', heading);
     let jsMessage = await driver.findElement(By.id('js-message')).getText();
