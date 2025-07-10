5d55309b3b92b569de1a3b94bdbdb4114bf869a0
Dominik Skowron <56132989+donnik007@users.noreply.github.com>
2025-07-10 09:14:27 +0000

    xd

--- tests/test_app.js
+++ tests/test_app.js
@@ -1,9 +1,9 @@
 const {Builder, By, until} = require('selenium-webdriver');
 (async function example() {
-  // Use localhost since PHP server runs on 127.0.0.1:8000 in CI
-  let driver = await new Builder().forBrowser('chrome').usingServer('http://localhost:4444/wd/hub').build();
+  // Use service hostnames for Docker Compose network
+  let driver = await new Builder().forBrowser('chrome').usingServer('http://selenium:4444/wd/hub').build();
   try {
-    await driver.get('http://127.0.0.1:8000');
+    await driver.get('http://php:8000');
     let heading = await driver.findElement(By.tagName('h1')).getText();
     console.log('Heading:', heading);
     let jsMessage = await driver.findElement(By.id('js-message')).getText();
